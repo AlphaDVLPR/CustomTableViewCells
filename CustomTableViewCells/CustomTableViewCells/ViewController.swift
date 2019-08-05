ca324be80d724536be87759fd560397af24bc480
AlphaDVLPR <alphadvlpr@AlphaDVLPRs-MacBook-Pro.local>
2019-08-05 10:07:10 -0600

    Initial Commit

---
+++ CustomTableViewCells/CustomTableViewCells/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomTableViewCells
+//
+//  Created by AlphaDVLPR on 8/5/19.
+//  Copyright © 2019 AlphaDVLPR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
